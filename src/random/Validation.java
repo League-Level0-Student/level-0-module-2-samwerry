ed510153a9ea04f73ff70bc6b9e01c27cf7246c4
samwerry <league@WTS13.attlocal.net>
2018-11-06 16:59:54 -0800

    I finished Dice Roll, magic 8 ball, rock paper succors, validation, and started lottery numbers

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -8,18 +8,43 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class Validation {
+	
 	public static void main(String[] args) {
-		
+
 		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
 
-		// 1. Use each value of randomNumber to give the user a random compliment.
+		int randomNumber = 0;
 
+		for (int i = 0; i < 10; i++) {
+			randomNumber = randomMaker.nextInt(5);
+
+			System.out.println(randomNumber);
+			
+			if (randomNumber == 1) {
+
+				
+
+				// 1. Use each value of randomNumber to give the user a random compliment.
+
+				JOptionPane.showMessageDialog(null, "you have cool shoes");
+			}
+			if (randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "you have cool hair");
+			}
+			if (randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "you have cool cloths");
+			}
+			if (randomNumber == 4) {
+				JOptionPane.showMessageDialog(null, "you have cool arms");
+			}
+			if (randomNumber == 5) {
+				JOptionPane.showMessageDialog(null, "you have cool legs");
+			}
+
+		}
 		// 2. Repeat all the code above 10 times
-		
+
 		// 3. Find someone to test out your program. They will like it :)
 	}
+
 }
